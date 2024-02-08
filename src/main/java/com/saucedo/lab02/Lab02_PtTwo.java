8d9d3bdc6861b058f7e2ee58ec8bbe13509698c3
Benja Saucedo <benjaminsaucedo7@gmail.com>
2024-02-07 21:50:10 -0800

    Updated Lab 2 pt 2.

--- src/main/java/com/saucedo/lab02/Lab02_PtTwo.java
+++ src/main/java/com/saucedo/lab02/Lab02_PtTwo.java
@@ -16,6 +16,9 @@ public class Lab02_PtTwo {
 
     /** Return added sum of fractions for the specified number */
     public static double sumOfFrac(int n) {
+        if (n == 0){
+            return -1;
+        }
         if (n == 1) {
             // Base case
             return 1;
