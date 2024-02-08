fa23b925152d49f06fc997eca19f58eaa897f880
Benja Saucedo <benjaminsaucedo7@gmail.com>
2024-02-07 22:37:06 -0800

    Updated Lab 2 pt 5

---
+++ src/main/java/com/saucedo/lab02/Lab02_PtFive.java
@@ -0,0 +1,28 @@
+package com.saucedo.lab02;
+import java.util.Scanner;
+
+public class Lab02_PtFive {
+    //Compute Fibonacci
+    public static long count = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input");
+        int n = sc.nextInt();
+
+        long result = fib(n);
+        System.out.println("Fibonacci of " + n + " is: " + result);
+        System.out.println("Fibonacci was called "+ count + " time(s).");
+        sc.close();
+    }
+
+    private static long fib(int n) {
+        count++;
+        if(n<=1) {
+            return n;
+        }
+        else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
