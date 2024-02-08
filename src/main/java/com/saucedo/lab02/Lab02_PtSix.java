f367aab460c5c1410028659dd3797a4dfe86db4f
Benja Saucedo <benjaminsaucedo7@gmail.com>
2024-02-07 23:31:26 -0800

    Updated Lab 2 pt 6.1 complete

---
+++ src/main/java/com/saucedo/lab02/Lab02_PtSix.java
@@ -0,0 +1,30 @@
+package com.saucedo.lab02;
+
+import java.util.Scanner;
+
+public class Lab02_PtSix {
+    //main method
+    public static void main(String[] args) {
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a non negative integer: ");
+        int n = input.nextInt();
+
+        // Display Reverse Order
+        System.out.println("Original int: \n" + n);
+        System.out.println("Reverse Order: ");
+        reverseNumOrder(n);
+    }
+
+    public static void reverseNumOrder(int n) {
+        if (n < 10) {
+            // Base case
+            System.out.print(n);
+        } else {
+            System.out.print(n%10);
+            reverseNumOrder(n/10); // Recursive call
+        }
+    }
+
+
+}
